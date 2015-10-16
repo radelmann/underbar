9da6c93308448e88f7d53d3d1d8d212c63ff9882
radelmann <robadelmann@gmail.com>
2015-10-16 11:03:08 -0700

    Refactored Reduce

--- src/underbar.js
+++ src/underbar.js
@@ -156,15 +156,9 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
-
     _.each(collection, function(item) {
-      if (typeof(accumulator) !== 'undefined') {
-        accumulator = iterator(accumulator, item);
-      } else {
-        accumulator = item;
-      }
+      accumulator = accumulator === undefined ? item : iterator(accumulator, item);
     });
-
     return accumulator;
   };
 
